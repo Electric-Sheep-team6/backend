ad1c28ee65587bd1bc65df706caa2c55ab5b513a
saku0512 <saku0512sec@gmail.com>
2026-06-16 23:07:04 +0900

    helth server

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := router.Run(":" + port); err != nil {
+		panic(err)
+	}
+}
